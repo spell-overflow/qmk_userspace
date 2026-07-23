b082186960e51ecdc450e6b85c0d1c62c802d0af
Spelloverflow <mail@annekehugenberg.de>
2026-07-23 18:55:08 +0200

    first keyboard-config

---
+++ keyboards/splitkb/halcyon/corne/keymaps/rootwitchs-corne-layout/rules.mk
@@ -0,0 +1,11 @@
+ENCODER_MAP_ENABLE = yes
+
+# This adds module functionality to your keyboard (files found in users/halcyon_modules)
+USER_NAME := halcyon_modules
+SRC += halcyon_keys.c
+
+# Display module
+HLC_TFT_DISPLAY = yes
+
+# Cirque trackpad module
+HLC_CIRQUE_TRACKPAD = yes
